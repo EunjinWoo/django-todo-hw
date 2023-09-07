f81eb5d767819eb5fe188f9b40f0cd8cf5d03195
Eunjin Woo <esther0904@naver.com>
2023-09-07 16:50:54 +0900

    add User Model

--- user/models.py
+++ user/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
-# hi
+class UserModel(AbstractUser):
+
+    class Meta:
+        db_table = "my_user" # 여기는 테이블 이름이에요! 꼭 기억 해 주세요!
+
+    email = models.TextField(max_length=100)
