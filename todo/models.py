51e90d3aba70d3c195c99efd7cbd47e06e2ae2e9
Eunjin Woo <esther0904@naver.com>
2023-09-07 17:06:29 +0900

    add Todo Model

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
+# tweet/models.py
 from django.db import models
+from user.models import UserModel
+
 
 # Create your models here.
+class TodoModel(models.Model):
+    class Meta:
+        db_table = "todo"
+
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
